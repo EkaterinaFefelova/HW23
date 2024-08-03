ce40c0c948c5adadfdaae8d0fd8a70ae8aa9b69d
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-08-03 11:31:51 +0300

    HW23 completed

--- HW23/src/Main.java
+++ HW23/src/Main.java
@@ -1,4 +1,55 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
+
+    private static Set<String> numbers = new TreeSet<String>();
+
     public static void main(String[] args) {
+       while (true){
+           String input = getInput();
+           if(isValidNumber(input))
+               addNumber(formatNumber(input));
+            else if (input.equalsIgnoreCase("Печать"))
+                printAll();
+            else if (input.equalsIgnoreCase("Выход"))
+                break;
+            else
+                System.out.println("Неверный ввод!");
+       }
+    }
+
+    public static String getInput(){
+        System.out.println("Пожалуйста, введите российский номер телефона, команду Печать или Выход: ");
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static boolean isValidNumber (String input){
+        String result = input.replaceAll("[., +()-]", "");
+        if(result.length()==10)
+            result = "7" + result;
+        return result.matches("[7-8](9)[0-9]{9}+");
+    }
+
+    public static String formatNumber (String number){
+        String result = number.replaceAll("[., +()-]", "");
+        if(result.length()==10)
+            return result;
+        return result.substring(1);
+    }
+
+    public static void addNumber (String number){
+        if(numbers.add(number))
+            System.out.println("Номер успешно добавлен");
+       else
+            System.out.println("Такой номер уже есть в базе");
+    }
+
+    public static void printAll() {
+        if(numbers.isEmpty())
+            System.out.println("Cписок номеров пуст!");
+        for (String number : numbers)
+            System.out.println(number);
     }
 }
